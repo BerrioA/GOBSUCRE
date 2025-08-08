60ef318a2510ef3b88f5b9b2025aa5567c5d48bc
BerrioA <ingalejandroberrio@gmail.com>
2025-08-08 17:58:34 -0500

    Prueba Download Document

--- src/controllers/students/studentsController.js
+++ src/controllers/students/studentsController.js
@@ -168,7 +168,7 @@ export const getStudentById = async (req, res) => {
         student.practitioner_information?.program?.program_name || null,
       documents: student.documents.map((doc) => ({
         document: doc.document,
-        fileUrl: doc.fileUrl,
+        fileUrl: `${process.env.BASE_URL}/uploads/${doc.fileUrl}`,
         document_name: doc.document_type?.document_name || null,
       })),
     };
