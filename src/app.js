ff7281e71046ebcc2ebd06bf6cee0a115309d61d
BerrioA <ingalejandroberrio@gmail.com>
2025-08-08 17:44:25 -0500

    Ruta upload establecido como fija

--- src/app.js
+++ src/app.js
@@ -33,6 +33,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors(corsOptions));
 app.use(express.urlencoded({ extended: true }));
+app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
 
 // Rutas
 
